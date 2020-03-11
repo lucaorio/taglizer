d7f815bb585ff5c16dd4e4b445575165714c981a
Luca Orio <luca.o@me.com>
2020-03-10 22:37:26 -0700

    fixed const declaration

--- analyze/index.js
+++ analyze/index.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra')
 const chalk = require('chalk')
 const pug = require('pug')
 const untildify = require('untildify')
-var pkg = require('../package.json')
+const pkg = require('../package.json')
 const getDiscography = require('./get-discography')
 
 ;(async () => {
